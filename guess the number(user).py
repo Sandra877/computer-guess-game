afaf285c4cb2468a6957b0778c5e050bb7c1b9a7
Sandra877 <110770016+Sandra877@users.noreply.github.com>
2022-08-10 15:58:50 +0300

    Add files via upload

---
+++ guess the number(user).py
@@ -0,0 +1,20 @@
+import random
+
+def comp_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if  high != low:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high(H) , too low(L) , or correct(C)").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"The computer guessed the number {guess} correctly!")
+
+comp_guess(4000)
